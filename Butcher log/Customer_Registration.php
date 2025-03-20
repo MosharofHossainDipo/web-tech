752f65452d66968478641dce8283052938bf94fd
SADMANTANZIM <shabab.s.t198@gmail.com>
2025-03-20 09:19:03 +0600

    new

--- Butcher log/Customer_Registration.php
+++ Butcher log/Customer_Registration.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Customer Registration</title>
-    <link rel="stylesheet" type="text/css" href="../css/CRStyles.css"> 
+    <link rel="stylesheet" type="text/css" href="../css/CRStyles.css">
 </head>
 <body>
     <div class="background-image"></div>
